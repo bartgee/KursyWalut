30d985e35c5d51b4fcf8ed3a652b1662d2f89737
Bart Grzybicki <bgrzybicki@gmail.com>
2014-11-15 02:46:34 +0100

    added get_currencies function and some iterator

--- kursywalut.py
+++ kursywalut.py
@@ -10,20 +10,29 @@ import requests
 
 #OUT_FILE = u'linuxtoday.txt'
 
-def main():
+def get_currencies():
     page = requests.get('http://finanse.wp.pl/waluty.html')
     tree = html.fromstring(page.text)
     lt_list = []
     index = 1
+    curr_list = []
     while True:
-        #lt_dict = {}
+        curr_dict = {}
         curr = tree.xpath('//*[@id="wykresyWalutyT{}"]/a/span[1]/span/text()'.format(index))
         value = tree.xpath('//*[@id="wykresyWalutyT{}"]/a/span[2]/text()'.format(index))
         if value == []:
             break
         index += 1
         #print(curr)
-        print(curr[0] + ': ' + value[0])
+        curr_dict['currency'] = curr[0]
+        curr_dict['value'] = value[0]
+        curr_list.append(curr_dict)
+    return curr_list
+
+def main():
+    values = get_currencies()
+    for curr in values:
+        print(curr['currency'] + ': ' + curr['value'])
 
 if __name__ == '__main__':
     main()
