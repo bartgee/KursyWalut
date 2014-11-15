3916f449582a8e4809f2636abb051af520612956
Bart Grzybicki <bgrzybicki@gmail.com>
2014-11-15 03:57:01 +0100

    added get_exchg_rate function... but it's not complete yet... gotta get some sleep now... will continue in the morning :D

--- kursywalut.py
+++ kursywalut.py
@@ -10,6 +10,9 @@ import requests
 
 #OUT_FILE = u'linuxtoday.txt'
 
+def print_header():
+    print('$$$ KursyWalut ' + __version__ + ' $$$\n')
+
 def get_currencies():
     page = requests.get('http://finanse.wp.pl/waluty.html')
     tree = html.fromstring(page.text)
@@ -29,10 +32,34 @@ def get_currencies():
         curr_list.append(curr_dict)
     return curr_list
 
+def get_exchg_rate(curr_list):
+    exchg_list = []
+    for curr in curr_list:
+        exchg_dict = {}
+        try:
+            curr_idx_forex = curr['currency'].index('/')
+        except Exception:
+            pass
+        try:
+            curr_idx_nbp = curr['currency'].index(' (')
+        except Exception:
+            pass
+        try:
+            curr_explct = curr['currency'][:curr_idx_forex]
+            exchg_dict['currency_forex'] = curr_explct
+        except Exception:
+            curr_explct = curr['currency'][:curr_idx_nbp]
+            exchg_dict['currency_nbp'] = curr_explct
+        exchg_list.append(exchg_dict)
+    return exchg_list
+        #print_header('')
+
 def main():
+    print_header()
     values = get_currencies()
     for curr in values:
         print(curr['currency'] + ': ' + curr['value'])
+    print(get_exchg_rate(values))
 
 if __name__ == '__main__':
     main()
