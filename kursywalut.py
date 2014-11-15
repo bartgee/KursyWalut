2d71f71da213f6edf6536f0f69bfc2b628a3aeb4
Bart Grzybicki <bgrzybicki@gmail.com>
2014-11-15 02:29:55 +0100

    messy code cleanup

--- kursywalut.py
+++ kursywalut.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # author Bart Grzybicki <bgrzybicki@gmail.com>
-# version
 
 __author__ = 'Bart Grzybicki <bgrzybicki@gmail.com>'
+__version__ = '0.1'
 
 from lxml import html
 import requests
@@ -19,9 +19,6 @@ def main():
         #lt_dict = {}
         curr = tree.xpath('//*[@id="wykresyWalutyT{}"]/a/span[1]/span/text()'.format(index))
         value = tree.xpath('//*[@id="wykresyWalutyT{}"]/a/span[2]/text()'.format(index))
-        #//*[@id="wykresyWalutyT2"]/a/span[2]
-        #print(tree)
-        #print(type(value))
         if value == []:
             break
         index += 1
