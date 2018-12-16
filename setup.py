120a825e755c3bbbd61ab5914df43705d9dfb461
Bart Grzybicki <bgrzybicki@gmail.com>
2018-12-16 12:12:41 +0100

    Setup requirements updated.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['lxml>=4.2.5', 'requests>=2.21.0', 'six>=1.12.0', ]
 
 setup_requirements = ['pytest-runner', ]
 
