43bf0612dcca2a33dbb084d0c5256b3a1832aab9
Bart Grzybicki <bgrzybicki@gmail.com>
2018-12-20 10:51:50 +0100

    F401 issue added to one of the imports.

--- kursywalut/parsers/__init__.py
+++ kursywalut/parsers/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Various parsers package."""
 
-from .moneypl_parser import MoneyPlParser
+from .moneypl_parser import MoneyPlParser  # noqa, F401 flake8 issue
 
 __author__ = 'Bart Grzybicki'
 
